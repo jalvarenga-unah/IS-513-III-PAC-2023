81489f654699ef72640951af3b1073de11fdfd34
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2023-11-01 16:52:48 -0600

    Trabajando con imagenes y gestos

--- proyectos-flutter/componentes/lib/images_page.dart
+++ proyectos-flutter/componentes/lib/images_page.dart
@@ -9,7 +9,40 @@ class ImagesPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Imagenes'),
       ),
-      body: Column(children: []),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // imagen local
+
+            InkWell(
+              splashColor: Colors.red,
+              onTap: () {},
+              child: Image(
+                image: AssetImage('assets/images/logo.jpg'),
+                // width: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            GestureDetector(
+              onTap: () => print('presiono imagen'),
+              child: Image.asset('assets/images/logo.jpg'),
+            ),
+
+            //imagen de internet
+            const ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: Image(
+                image: NetworkImage(
+                    'https://th.bing.com/th/id/R.14732af8c12f63a81fc43b0e875781af?rik=zv4E6W5UvKq7ww&riu=http%3a%2f%2f4.bp.blogspot.com%2f-4cq-eUn8t3A%2fT3nADDmOOMI%2fAAAAAAAABng%2fkSjd_uSAuOo%2fs1600%2ffotos-perros-cachorros.jpg&ehk=ZZCh57TZQP500jH2Wd5YVx9XdkK5DLu0b79jemFqwxQ%3d&risl=&pid=ImgRaw&r=0'),
+                width: 300,
+                height: 350,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
